0100d7ee7ca8f2ba687500636e3affbbe628294f
JawadAmmar700 <JawadAmmarCode700@gmail.com>
2023-04-18 21:59:22 +0300

    testing calendar cron job

--- index.js
+++ index.js
@@ -2,11 +2,88 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const socket = require("socket.io");
-require("./monitor");
+// require("./monitor");
+const cron = require("node-cron");
+const { PrismaClient } = require("@prisma/client");
+const CronParser = require("cron-parser");
+
+const client = new PrismaClient();
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hey there!, this is a server that is used by the senior project");
+});
+
+const scheduleJobs = new Map();
+function dateToCron(date) {
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+  const dayOfMonth = date.getDate();
+  const month = date.getMonth() + 1; // add 1 because January is 0
+  const dayOfWeek = date.getDay();
+  const year = date.getFullYear();
+
+  // format into a cron expression string
+  const cronExpression = `${minutes} ${hours} ${dayOfMonth} ${month} ${dayOfWeek}`;
+  return cronExpression;
+}
+
+app.post("/", (req, res) => {
+  const todo = req.body;
+  // calculate the time 10 minutes before todo.time
+  // const scheduledTime = new Date(todo.time - 600);
+  const date2 = new Date(todo.date);
+  const year = date2.getFullYear();
+  const month = (date2.getMonth() + 1).toString().padStart(2, "0"); // add zero-padding to month
+  const day = date2.getDate().toString().padStart(2, "0"); // add zero-padding to day
+  const formattedDate2 = `${year}-${month}-${day}`;
+
+  const unixTimestamp = todo.time * 1000;
+  const date = new Date(unixTimestamp - 600000);
+  const timeString = date.toLocaleTimeString("en-US", {
+    timeZone: "Europe/Istanbul",
+    hour12: false,
+  });
+
+  const scheduledTime = new Date(`${formattedDate2}T${timeString}`);
+  const cronSchedule = dateToCron(scheduledTime);
+
+  console.log("cronSchedule", cronSchedule);
+
+  const job = cron.schedule(
+    cronSchedule,
+    async () => {
+      console.log("running a task once at", todo.title);
+
+      await client.reminder.update({
+        where: {
+          id: todo.id,
+        },
+        data: {
+          notificationSent: true,
+        },
+      });
+      // destroy the job
+      if (scheduleJobs.has(todo.id)) {
+        console.log("destroying the job");
+        scheduleJobs.get(todo.id).stop();
+        scheduleJobs.delete(todo.id);
+      }
+    },
+    {
+      scheduled: false,
+    }
+  );
+
+  if (!scheduleJobs.has(todo.id)) {
+    scheduleJobs.set(todo.id, job);
+    job.start();
+  }
+  res.json({
+    message: "the cron is running",
+  });
 });
 
 app.use(cors({ origin: process.env.CLIENT_APP, optionsSuccessStatus: 200 }));
